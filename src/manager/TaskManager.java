bea3737d2cbc2a469fe58c3c8d867f09fc626cb6
MaratID <vasuu-1982@yandex.ru>
2025-08-16 01:31:44 +0300

    Задание по ТЗ Спринт 6 (испр-3)

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 
 public interface TaskManager {
     //получить список Задач
+
     ArrayList<Task> getTaskList();
 
     //получить список Эпиков
+
     ArrayList<Epictask> getEpicTaskList();
 
     //получить список подзадач
+
     ArrayList<Subtask> getSubTaskList();
 
     void clearTasks();
@@ -21,6 +24,7 @@ public interface TaskManager {
     void clearSubtasks();
 
     //получение по Id
+
     Task getTaskById(int id);
 
     Epictask getEpictaskById(int id);
@@ -28,23 +32,27 @@ public interface TaskManager {
     Subtask getSubtaskById(int id);
 
     //создание Задач
+
     void createTask(Task task);
 
     void createEpicTask(Epictask epictask);
 
     void createSubtask(Subtask subtask);
     //обновление задач
+
     void renewTask(Task task);
 
     void renewEpictask(Epictask epictask);
 
     void renewSubtask(Subtask subtask);
     //удаление по id
+
     void deleteTaskById(int id);
 
     void deleteEpictaskById(int id);
 
     void deleteSubtaskById(int id);
+
     public ArrayList<Task> getHistory();
 
 }
